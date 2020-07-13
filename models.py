e4491596c6ccbc19e372e3d53042e34a75459996
Raunak Tamang <raunaktamang@gmail.com>
2020-07-13 22:24:00 +0530

    final

--- models.py
+++ models.py
@@ -11,3 +11,5 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     hashed_pswd = db.Column(db.String(), nullable=False)
+
+    db.create_all()
